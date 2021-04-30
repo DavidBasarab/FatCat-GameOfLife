2b8c580520c38f859371349b8eda81b12e6d2c3c
David Basarab <dbasarab617@gmail.com>
2021-04-30 18:55:41 -0400

    game backcolor is blue

--- FatCat.GameOfLife/Game.cs
+++ FatCat.GameOfLife/Game.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -7,6 +9,21 @@ namespace FatCat.GameOfLife
 	public class Game : GameWindow
 	{
 		public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) { }
+
+		protected override void OnLoad()
+		{
+			GL.ClearColor(Color.DeepSkyBlue);
+			
+			base.OnLoad();
+		}
+
+		protected override void OnRenderFrame(FrameEventArgs args)
+		{
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+			
+			Context.SwapBuffers();
+			base.OnRenderFrame(args);
+		}
 
 		protected override void OnUpdateFrame(FrameEventArgs args)
 		{
