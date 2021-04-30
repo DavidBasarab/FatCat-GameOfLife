dee48cc50e6e1b43cc3a7c8325370b7a7cfd6d48
David Basarab <dbasarab617@gmail.com>
2021-04-30 18:51:44 -0400

    working now

--- FatCat.GameOfLife/Game.cs
+++ FatCat.GameOfLife/Game.cs
@@ -1,9 +1,21 @@
+using FatCat.GameOfLife.Utilities;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace FatCat.GameOfLife
 {
 	public class Game : GameWindow
 	{
 		public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) { }
+
+		protected override void OnUpdateFrame(FrameEventArgs args)
+		{
+			Log.Debug($"In OnUpdate Frame Escape Down = <{KeyboardState.IsKeyDown(Keys.Escape)}>");
+			
+			if (KeyboardState.IsKeyDown(Keys.Escape)) Close();
+
+			base.OnUpdateFrame(args);
+		}
 	}
 }
